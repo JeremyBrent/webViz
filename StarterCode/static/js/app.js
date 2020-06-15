1d09ce89f44fc41b1ca50f912a262023bea1a558
JeremyBrent <jeremybrentdata@gmail.com>
2020-06-14 23:23:25 -0400

    continuing to work on bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,15 +13,17 @@ d3.json("samples.json").then(function(data) {
 
     var dropDownValue = dropDown.node().value;
 
-    var otuIds = data.samples[0].otu_ids;
+    var otuIds = data.samples[0].otu_ids.slice(0,10).reverse();
+
 
     var sampleValues = data.samples[0].sample_values;
     var sortSampleValues= sampleValues.sort((a,b) => b - a);
     var topSampleValues = sortSampleValues.slice(0,10).reverse();
 
     var otuLabels = data.samples[0].otu_lables;
-    console.log(topSampleValues)
-
+/////////
+// Need to get OTU labels for y axis //
+/////////
     data = [{
         x: topSampleValues, 
         y: `OTU ${otuIds}`,
@@ -29,9 +31,37 @@ d3.json("samples.json").then(function(data) {
         orientation:'h'
     }];
 
-    Plotly.newPlot("bar",data);
+    layout = {
+        title: {
+            text: `Top ten species in Subject ${dropDownValue}`
+        },
+        xaxis: {
+            title: {
+                text: "Sample Values"
+            }
+        },
+        yaxis: {
+            title: {
+                text: "OTU"
+            }
+        }
+    };
+
+    Plotly.newPlot("bar",data,layout);
 
 });
 
 
 // function optionChanged();
+
+ // var sortable = [];
+    // for (var sample in samples ) {
+    //     sortable.push([sample,samples[sample]]);
+    // }
+    // sortable.sort((a,b) => b-a);
+    // console.log(sortable)
+
+    // var topSampleValues = sortable[2][0].slice(0,10).reverse()
+    // var otuIds = sortable[3].slice(0,10).reverse()
+
+    // console.log(topSampleValues)
