dfd93058b9f14db9e3dc9d7dc60177185c076696
JeremyBrent <jeremybrentdata@gmail.com>
2020-06-14 22:28:35 -0400

    completed dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,26 @@
-// var data = d3.json("../sample.json")
-// console.log(data)
+d3.json("samples.json").then(function(data) {
+    console.log(data);
+    var names = data.names;
+    // console.log(names);
+    var otuIds = data.samples.otu_ids;
+    var sampleValues = data.samples.sample_values;
+    var otuLabels = data.samples.otu_lables;
+    console.log(sampleValues)
+
+    // Selecting dropdown menu
+    var dropDown = d3.select("#selDataset")
+    
+    // adding each name label to the dropdown menu
+    names.forEach((name) => dropDown.append("option").text(name))
+    
+    // trace1 = {
+    //     x: , 
+    //     y: ,
+    //     type: 'bar',
+
+    // }
+
+});
 
 
-var data = 
+// function optionChanged();
