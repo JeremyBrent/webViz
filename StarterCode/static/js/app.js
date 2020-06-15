1cd5a342a035180685d4db71c6139ba80ed04a99
JeremyBrent <jeremybrentdata@gmail.com>
2020-06-15 00:01:39 -0400

    added dempgraphic information

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,32 +3,48 @@ d3.json("samples.json").then(function(data) {
     var names = data.names;
     // console.log(names);
     
-    console.log(sampleValues);
+    /////
+    // Demographic info 
+    /////
+    var metaData = data.metadata[0]
+    var metaDataDiv = d3.select("#sample-metadata")
+    Object.entries(metaData).forEach(([key,value]) => metaDataDiv.append("p").html( `<strong>${key.toUpperCase()}:</strong> ${value}`))
+    // Demographic info complete
+    /////
 
-    // Selecting dropdown menu
+    //////
+    // Dropdown menu
+    /////
     var dropDown = d3.select("#selDataset");
     
     // adding each name label to the dropdown menu
     names.forEach((name) => dropDown.append("option").text(name).attr('value',name));
 
     var dropDownValue = dropDown.node().value;
+    // Dropdown Menu Complete
+    //////
 
-    var otuIds = data.samples[0].otu_ids.slice(0,10).reverse();
-
-
+    //////
+    // Bar Chart 
+    /////
+    // Sample Values
     var sampleValues = data.samples[0].sample_values;
     var sortSampleValues= sampleValues.sort((a,b) => b - a);
     var topSampleValues = sortSampleValues.slice(0,10).reverse();
-
-    var otuLabels = data.samples[0].otu_lables;
-/////////
-// Need to get OTU labels for y axis //
-/////////
-    data = [{
+    // OTU ID
+    var otuIds = data.samples[0].otu_ids.slice(0,10).reverse();
+    // OTU Label
+    var otuLabels = data.samples[0].otu_labels.slice(0,10).reverse();
+    /////////
+    // Need to get OTU labels for y axis //
+    /////////
+    barData = [{
         x: topSampleValues, 
         y: `OTU ${otuIds}`,
         type: 'bar',
-        orientation:'h'
+        orientation:'h',
+        text: otuLabels,
+        label: otuIds
     }];
 
     layout = {
@@ -36,32 +52,18 @@ d3.json("samples.json").then(function(data) {
             text: `Top ten species in Subject ${dropDownValue}`
         },
         xaxis: {
-            title: {
-                text: "Sample Values"
-            }
+            title: "Sample Values",
+            automargin: true
         },
         yaxis: {
-            title: {
-                text: "OTU"
-            }
+            title: "OTU",
+            automargin: true
         }
     };
 
-    Plotly.newPlot("bar",data,layout);
+    Plotly.newPlot("bar",barData,layout);
 
+
+    
 });
 
-
-// function optionChanged();
-
- // var sortable = [];
-    // for (var sample in samples ) {
-    //     sortable.push([sample,samples[sample]]);
-    // }
-    // sortable.sort((a,b) => b-a);
-    // console.log(sortable)
-
-    // var topSampleValues = sortable[2][0].slice(0,10).reverse()
-    // var otuIds = sortable[3].slice(0,10).reverse()
-
-    // console.log(topSampleValues)
