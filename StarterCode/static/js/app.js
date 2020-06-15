dbf962452e7630a2c4e33dbdad253609247efc7d
JeremyBrent <jeremybrentdata@gmail.com>
2020-06-14 22:58:34 -0400

    added bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,23 +2,34 @@ d3.json("samples.json").then(function(data) {
     console.log(data);
     var names = data.names;
     // console.log(names);
-    var otuIds = data.samples.otu_ids;
-    var sampleValues = data.samples.sample_values;
-    var otuLabels = data.samples.otu_lables;
-    console.log(sampleValues)
+    
+    console.log(sampleValues);
 
     // Selecting dropdown menu
-    var dropDown = d3.select("#selDataset")
+    var dropDown = d3.select("#selDataset");
     
     // adding each name label to the dropdown menu
-    names.forEach((name) => dropDown.append("option").text(name))
-    
-    // trace1 = {
-    //     x: , 
-    //     y: ,
-    //     type: 'bar',
+    names.forEach((name) => dropDown.append("option").text(name).attr('value',name));
 
-    // }
+    var dropDownValue = dropDown.node().value;
+
+    var otuIds = data.samples[0].otu_ids;
+
+    var sampleValues = data.samples[0].sample_values;
+    var sortSampleValues= sampleValues.sort((a,b) => b - a);
+    var topSampleValues = sortSampleValues.slice(0,10).reverse();
+
+    var otuLabels = data.samples[0].otu_lables;
+    console.log(topSampleValues)
+
+    data = [{
+        x: topSampleValues, 
+        y: `OTU ${otuIds}`,
+        type: 'bar',
+        orientation:'h'
+    }];
+
+    Plotly.newPlot("bar",data);
 
 });
 
